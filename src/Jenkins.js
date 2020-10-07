0db289e5c16a1e7f14353ba8b95eba1e0a4e9ac6
TerryLee <Terry.lee@ehsn.com.tw>
2020-10-07 15:08:03 +0800

    update code

---
+++ src/Jenkins.js
@@ -0,0 +1,126 @@
+
+const {
+    By,
+    Key,
+    until
+} = require('selenium-webdriver');
+const assert = require('assert');
+const {
+    URL,
+    BRANCH,
+    USER_NAME,
+    PASS_WORD
+} = require('./config')
+
+const page = {
+    title: 'Lab_Publish_New [EHS » Web] [Jenkins]',
+    selectors: {
+        loginLink: '.login a',
+        loginBtn: 'yui-gen1-button',
+        username: 'j_username',
+        password: 'j_password',
+        buildLinkText: 'Build with Parameters',
+        buildSelect: 'select',
+        branchOption: `option[value="${BRANCH}"]`,
+    }
+}
+const selectors = page.selectors
+
+class Jenkins {
+    static init (driver) {
+        global.driver = driver
+        driver.manage().window().maximize()
+        it('開啟 lab jenkins', (done) => {
+            (async () => {
+                try {
+                    await driver.get(URL);
+                    const title = await driver.getTitle()
+                    await assert.equal(title, page.title)
+                    await done()
+                } catch (error) {
+                    await done(error)
+                }
+            })()
+        })
+    }
+    static login () {
+        it('點擊 [登入] 按鈕', (done) => {
+            (async () => {
+                await driver.findElement(By.css(selectors.loginLink)).click();
+                await done()
+            })()
+        })
+        it('是否在 [登入] 頁面', (done) => {
+            (async () => {
+                try {
+                    const element = await driver.findElement(By.id(selectors.loginBtn));
+                    await assert.notEqual(element, null)
+                    await done()
+                }
+                catch (error) {
+                    await done(error)
+                    throw('沒有登入按鈕！')
+                }
+            })()
+        })
+        it('輸入 [使用者帳號]、[密碼]', (done) => {
+            (async () => {
+                await setTimeout(async () => {
+                    // 使用者帳號
+                    await driver.findElement(By.name(selectors.username)).sendKeys(USER_NAME);
+                    // 密碼
+                    await driver.findElement(By.name(selectors.password)).sendKeys(PASS_WORD, Key.RETURN);
+                    await done()
+                }, 500);
+            })()
+        })
+        it('回到 lab jenkins 首頁', (done) => {
+            (async () => {
+                const title = await driver.getTitle()
+                await assert.equal(title, page.title)
+                await done()
+            })()
+        })
+    }
+    static build () {
+        it('點擊文字 [Build with Parameters] 連結', (done) => {
+            (async () => {
+                await driver.findElement(By.linkText(selectors.buildLinkText)).click();
+                await done()
+            })()
+        })
+        it('是否在 [Pipeline Lab_Publish_New] 頁面', (done) => {
+            (async () => {
+                try {
+                    const element = await driver.findElement(By.id(selectors.buildSelect));
+                    await assert.notEqual(element, null)
+                }
+                catch (error) {
+                    throw('沒有選 branch 的 select！')
+                }
+                await done()
+            })()
+        })
+        it(`選擇 [branch]: ${BRANCH}`, (done) => {
+            (async () => {
+                const selectElem = await driver.findElement(By.id(selectors.buildSelect));
+                await selectElem.click();
+                await driver.wait(until.elementLocated(By.css(selectors.branchOption)), 30 * 1000)
+                await selectElem.findElement(By.css(selectors.branchOption)).click()
+                await done()
+            })()
+        })
+        it('點擊 [建置] 按鈕', (done) => {
+            (async () => {
+                await driver.sleep(2000)
+                const selectElem = await driver.findElement(By.name('Submit'))
+                await selectElem.click()
+                await done()
+            })()
+        })
+    }
+}
+
+module.exports = {
+    Jenkins
+}
--- src/index.js
+++ src/index.js
@@ -1,107 +1,18 @@
 const {
-    Builder,
-    By,
-    Key
+    Builder
 } = require('selenium-webdriver');
 
-const {
-    URL,
-    BRANCH,
-    USER_NAME,
-    PASS_WORD
-} = require('./config')
+const { Jenkins } = require('./Jenkins')
 
-const assert = require('assert');
-let driver = new Builder().forBrowser('chrome').build();
-driver.manage().window().maximize()
+const driver = new Builder().forBrowser('chrome').build();
 
 describe('自動上版', () => {
-    it('開啟 lab jenkins', function (done) {
-        this.timeout(10000);
-        (async () => {
-            await driver.get(URL);
-            const title = await driver.getTitle()
-            await assert.equal(title, 'Lab_Publish_New [EHS » Web] [Jenkins]')
-            await done()
-        })()
-    })
-    it('點擊 [登入] 按鈕', function (done) {
-        (async () => {
-            await driver.findElement(By.css('.login a')).click();
-            await done()
-        })()
-    })
-    it('是否在 [登入] 頁面', function (done) {
-        (async () => {
-            try {
-                const element = await driver.findElement(By.id('yui-gen1-button'));
-                await assert.notEqual(element, null)
-            }
-            catch (error) {
-                throw('沒有登入按鈕！')
-            }
-            await done()
-        })()
-    })
-    it('輸入 [使用者帳號]、[密碼]', function (done) {
-        (async () => {
-            await setTimeout(async () => {
-                // 使用者帳號
-                await driver.findElement(By.name('j_username')).sendKeys(USER_NAME);
-                // 密碼
-                await driver.findElement(By.name('j_password')).sendKeys(PASS_WORD, Key.RETURN);
-                await done()
-            }, 500);
-        })()
-    })
-    it('回到 lab jenkins 首頁', function (done) {
-        (async () => {
-            const title = await driver.getTitle()
-            await assert.equal(title, 'Lab_Publish_New [EHS » Web] [Jenkins]')
-            await done()
-        })()
-    })
-    it('點擊 [Build with Parameters] 連結', function (done) {
-        (async () => {
-            await driver.findElement(By.linkText('Build with Parameters')).click();
-            await done()
-        })()
-    })
-    it('是否在 [Pipeline Lab_Publish_New] 頁面', function (done) {
-        (async () => {
-            try {
-                const element = await driver.findElement(By.id('select'));
-                await assert.notEqual(element, null)
-            }
-            catch (error) {
-                throw('沒有選 branch 的 select！')
-            }
-            await done()
-        })()
-    })
-    it(`選擇 [branch]: ${BRANCH}`, function (done) {
-        (async () => {
-            const selectElem = await driver.findElement(By.name('value'))
-            await selectElem.click();
-            async function waitUntilBranchSelectAppear () {
-                try {
-                    await selectElem.findElement(By.css(`option[value="${BRANCH}"]`)).click()
-                } catch (error) {
-                    // 沒出現，再等一下~
-                    setTimeout(() => {
-                        waitUntilBranchSelectAppear()
-                    }, 100);
-                }
-            }
-            await waitUntilBranchSelectAppear()
-            await done()
-        })()
-    })
-    it('點擊 [建置] 按鈕', function (done) {
-        (async () => {
-            const selectElem = await driver.findElement(By.name('Submit'))
-            await selectElem.click();
-            await done()
-        })()
+    Jenkins.init(driver)
+    Jenkins.login()
+    Jenkins.build()
+    after(() => {
+        setTimeout(() => {
+            driver.quit()
+        }, 5000);
     })
 })
