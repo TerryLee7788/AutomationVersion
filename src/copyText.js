7eef59c216fba9bc3ca4fe9c479c88496826452f
TerryLee <Terry.lee@ehsn.com.tw>
2020-10-22 14:56:00 +0800

    Add "BUFFER_MIN" value

--- src/copyText.js
+++ src/copyText.js
@@ -5,12 +5,13 @@ const {
     MINUTE
 } = require('./config')
 
-const delayTime = MINUTE * 60 * 1000
+const BUFFER_MIN = 1
+const delayTime = (+MINUTE + BUFFER_MIN) * 60 * 1000
 
 const nowTimeStamp = new Date().getTime()
 const buildTime = new Date(nowTimeStamp + delayTime)
 
 const buildHour = buildTime.getHours()
-const buildMinutes = buildTime.getMinutes()
+const buildMinutes = buildTime.getMinutes() < 10 ? '0' + buildTime.getMinutes() : buildTime.getMinutes()
 
 clipboardy.writeSync(`@B2C_RD '${BRANCH}' 分支預計 ${buildHour}:${buildMinutes} 推上 LAB 區`);
