62f95720295f09de9f2b1139f9cb66e6231bcf9f
TerryLee <Terry.lee@ehsn.com.tw>
2022-04-27 14:00:05 +0800

    更新 'chromedriver' 版本 更新 'sh/build.sh' sh 程式, 每次都更新 'chromedriver' 最新版 更新 'src/copyText.js', 新增一行 console 辨別當前上板的 branch

--- sh/build.sh
+++ sh/build.sh
@@ -1,1 +1,5 @@
+# 更新 chromedriver 版本
+npm i --save-dev chromedriver@latest
+
+# 執行整合測試
 npm run test:mocha
--- src/copyText.js
+++ src/copyText.js
@@ -14,4 +14,5 @@ const buildTime = new Date(nowTimeStamp + delayTime)
 const buildHour = buildTime.getHours()
 const buildMinutes = buildTime.getMinutes() < 10 ? '0' + buildTime.getMinutes() : buildTime.getMinutes()
 
+console.log(`預計上版分支: ${BRANCH}`)
 clipboardy.writeSync(`@B2C_RD '${BRANCH}' 分支預計 ${buildHour}:${buildMinutes} 推上 LAB 區`);
